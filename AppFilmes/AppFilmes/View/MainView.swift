e0aa69f97deb28a7e637d47c8e2474f89331922f
Bruno Rocha <brunorocha.ti2010@gmail.com>
2021-09-06 18:34:14 -0300

    architecture configuration

---
+++ AppFilmes/AppFilmes/View/MainView.swift
@@ -0,0 +1,21 @@
+//
+//  MainView.swift
+//  AppFilmes
+//
+//  Created by Bruno Rocha on 06/09/21.
+//
+
+import UIKit
+
+class MainView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .gray
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
